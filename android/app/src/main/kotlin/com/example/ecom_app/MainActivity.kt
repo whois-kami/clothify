a6f15029eaf61c99205f669bcaf199e14375c846
Kamidashi <lipatovb@gmail.com>
2024-08-05 23:54:21 +0300

    deletion of sensitive data

--- android/app/src/main/kotlin/com/example/ecom_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/ecom_app/MainActivity.kt
@@ -4,10 +4,11 @@ import com.yandex.mapkit.MapKitFactory
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
+import com.example.ecom_app.BuildConfig
 
 class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        MapKitFactory.setApiKey("3f58f294-eedc-4523-983c-542da99950da")
+        MapKitFactory.setApiKey(BuildConfig.YANDEX_API_KEY)
         GeneratedPluginRegistrant.registerWith(flutterEngine)
     }
 }
